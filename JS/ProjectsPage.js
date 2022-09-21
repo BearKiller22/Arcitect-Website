7df22ebd4649fce1f0710536736cf37f01b0ae97
3vomanx <97153803+3vomanx@users.noreply.github.com>
2022-09-21 20:17:42 +0400

    Add files via upload

---
+++ JS/ProjectsPage.js
@@ -0,0 +1,31 @@
+// $(document).on('mouseenter', '.item', function(){
+//     $("p").slideDown("fast")
+// })
+// $(document).on('mouseleave', '.item', function(){
+//     $("p").slideUp("slow")
+// })
+
+
+$(document).ready(function(){
+    $(".switch").click(function(){
+        $("body").toggleClass("dark-mode");
+    if($("body").hasClass("dark-mode")){
+        $(".switch").attr("name", "sunny-outline");
+    }else{
+        $(".switch").attr("name", "moon-outline"); 
+    }
+  });
+});
+
+
+$(window).scroll(function(){
+    $('nav').toggleClass('scrolled', $(this).scrollTop() > 60)
+});
+
+//
+
+getdetails = () => {
+    var id = this.Attr('id')
+    var data = Details(id);
+    document.getElementById('description').innerHTML(data.description)
+}
